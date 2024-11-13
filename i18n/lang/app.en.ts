d55f4897405ae0eb7d5a5f1a43aa9b793459939d
Joseph GHAE <josephghae@MacBook-Air.local>
2024-11-13 15:25:47 +0100

    Initial commit

---
+++ i18n/lang/app.en.ts
@@ -0,0 +1,36 @@
+const translation = {
+  common: {
+    welcome: 'Salut !',
+    appUnavailable: 'App is unavailable',
+    appUnkonwError: 'App is unavailable',
+  },
+  chat: {
+    newChat: 'Nouveau chat',
+    newChatDefaultName: 'Nouvelle conversationn',
+    openingStatementTitle: 'Opening statement',
+    powerBy: 'Powered by',
+    prompt: 'Prompt',
+    privatePromptConfigTitle: 'Conversation settings',
+    publicPromptConfigTitle: 'Initial Prompt',
+    configStatusDes: 'Before start, you can modify conversation settings',
+    configDisabled:
+      'Previous session settings have been used for this session.',
+    startChat: 'Commencer',
+    privacyPolicyLeft:
+      'Please read the ',
+    privacyPolicyMiddle:
+      'privacy policy',
+    privacyPolicyRight:
+      ' provided by the app developer.',
+  },
+  errorMessage: {
+    valueOfVarRequired: 'Variables value can not be empty',
+    waitForResponse:
+      'Please wait for the response to the previous message to complete.',
+  },
+  variableTable: {
+    optional: 'Optional',
+  },
+}
+
+export default translation
